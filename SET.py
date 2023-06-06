ab614a44489ea32e30bf1f92bacef6699922b6e7
bhumii123 <111906536+bhumii123@users.noreply.github.com>
2023-06-06 14:24:57 +0530

    SET.py

--- SET.py
+++ SET.py
@@ -1,4 +1,9 @@
-setA=set()
+#Problem Statement:
+#To create ADT that implements the "set" concept. a. Add (new Element) -Place a value into the set ,
+#b. Remove (element) Remove the value c. Contains (element) Return true if element is in collection,
+#d. Size () Return number of values in collection Iterator () Return an iterator used to loop over
+#collection, e. Intersection of two sets , f. Union of two sets, g. Difference between two sets, h. Subset
+setA=set() 
 setB=set()
 n=int(input("enter the limit of setA:"))
 m=int(input("enter the limit of setB:"))
@@ -7,6 +12,7 @@ def add():
 		ele=input("enter element for setA:")
 		setA.add(ele)
 		print(setA);
+		
 def add1():
 	for i in range(m):
 		ele=input("enter element for setB:")
